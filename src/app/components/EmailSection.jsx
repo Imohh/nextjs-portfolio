80da3f4d8fdbf39856631758b58012ec58a0364a
imohh <jaypee88830@gmail.com>
2025-04-26 11:50:04 +0100

    linkedin and github icons removed

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -57,15 +57,15 @@ const EmailSection = () => {
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="github.com">
+          {/*<Link href="github.com">
             <Image src={GithubIcon} alt="Github Icon" />
-          </Link>
+          </Link>*/}
           <Link href="https://instagram.com/oprime.ng">
             <Image src={InstagramIcon} alt="Instagram Icon" />
           </Link>
-          <Link href="linkedin.com">
+          {/*<Link href="linkedin.com">
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
-          </Link>
+          </Link>*/}
         </div>
       </div>
       <div>
